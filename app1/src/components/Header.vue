87ff664c0323947155349fed384aae91fdbb829d
tacy <oyai32@126.com>
2023-04-08 22:30:09 +0800

    feat: app1 中增加 dayjs

--- app1/src/components/Header.vue
+++ app1/src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <h1>{{ name }}</h1>
+    <div>使用 dayjs：{{ day }}</div>
   </div>
 </template>
 <script>
+import dayjs from 'dayjs';
 
 export default {
   props: {
@@ -11,6 +13,14 @@ export default {
       type: String,
       default: '',
     },
+  },
+  data() {
+    return {
+      day:''
+    };
+  },
+  async mounted() {
+    this.day = dayjs().format('YYYY-MM-DD');
   },
 };
 </script>
