8ac49d50e768944561cddda0202249fd671c728a
tacy <oyai32@126.com>
2023-04-08 23:34:18 +0800

    feat: app1 的 Header 组件调用接口

--- app1/src/components/Header.vue
+++ app1/src/components/Header.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{ name }}</h1>
     <div>使用 dayjs：{{ day }}</div>
+    <div>{{ message }}</div>
   </div>
 </template>
 <script>
@@ -16,11 +17,15 @@ export default {
   },
   data() {
     return {
-      day:''
+      day:'',
+      message: '',
     };
   },
   async mounted() {
     this.day = dayjs().format('YYYY-MM-DD');
+    this.message = await fetch('localhost:8090/getData')
+      .then(response => response.json())
+      .catch(error => this.error = error)
   },
 };
 </script>
