b1c7fb246d83d752ec0f0d21eb4cbadcb8cba3e2
tacy <oyai32@126.com>
2023-04-08 22:59:35 +0800

    feat: app1 抛出 dayjs 给 app2 使用

--- app2/src/App.vue
+++ app2/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header name="app2，使用 app1 的 header"/>
+    <div>app2 dayjs：{{ day }}</div>
   </div>
 </template>
 <script>
@@ -8,6 +9,16 @@
 export default {
   components: {
     Header: () => import('app1/Header')
+  },
+  data() {
+    return {
+      day:''
+    };
+  },
+  async mounted() {
+    import('app1/dayjs').then(({default:dayjs})=>{
+      this.day = dayjs().format('YYYY-MM-DD');
+    })
   },
 }
 </script>
